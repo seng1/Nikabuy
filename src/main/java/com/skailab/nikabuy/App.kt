a868bb1b449b99692c5442b27f96ddd61c0cbae3
Seng <sorksengorng@gmail.com>
2020-10-09 21:56:09 +0700

    test

--- src/main/java/com/skailab/nikabuy/App.kt
+++ src/main/java/com/skailab/nikabuy/App.kt
@@ -3,12 +3,14 @@ package com.skailab.nikabuy
 import android.app.Application
 import android.content.Context
 import android.content.res.Resources
+import androidx.appcompat.app.AppCompatDelegate
 import com.skailab.nikabuy.LocaleHelper.onAttach
 
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         instance = this
         res = resources
     }
